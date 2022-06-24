82a7fd298e949e5cee54048c10b73d46860b5fd3
Claudio Campos <91580429+claudiohpcampos@users.noreply.github.com>
2022-06-24 00:21:24 -0300

    Update script.js

--- exercicios/ex013/modelo/script.js
+++ exercicios/ex013/modelo/script.js
@@ -12,6 +12,10 @@ function contar() {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando passo 1')
+            p = 1
+        }
         if (i < f) {
             for(let c = i; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1F449} `
